2c7640cf0e6385b2aabbb047f19398ed2594c350
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-05-08 10:14:25 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from 'http';
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Livros",
+    "/autores": "Autores",
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(rotas[req.url] || 'Rota não encontrada');
+});
+
+server.listen(PORT, () => {
+    console.log('Server running at http://localhost:3000/');
+});
