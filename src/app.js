5aeac51ebc5a326690b263df5dc54e5c1331fc6a
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-05-10 13:47:52 -0300

    :sparkles: feat: rotas ok e adicionando lib do mongodb

--- src/app.js
+++ src/app.js
@@ -6,20 +6,24 @@ app.use(express.json()); //Middleware para o express entender JSON
 const livros = [
     {
         id: 1,
-        nome: "O Senhor dos Anéis",
+        titulo: "O Senhor dos Anéis",
         autor: "J. R. R. Tolkien"
     },
     {
         id: 2,
-        nome: "Fundação",
+        titulo: "Fundação",
         autor: "Isaac Asimov"
     },
     {
         id: 3,
-        nome: "Duna",
+        titulo: "Duna",
         autor: "Frank Herbert"
     }
 ];
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => livro.id === id);
+}
 
 
 app.get("/", (req, res) => {
@@ -31,8 +35,8 @@ app.get("/livros", (req, res) => {
 });
 
 app.get("/livros/:id", (req, res) => {
-    const id = req.params.id;
-    const livro = livros.find(l => l.id == id);
+    const id = +req.params.id;
+    const livro = buscaLivro(id);
     if (!livro) {
         res.status(404).send("Livro não encontrado");
     }
@@ -45,6 +49,27 @@ app.post("/livros", (req, res) => {
     res.status(201).send("Livro adicionado com sucesso");
 });
 
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(+req.params.id);
+    if (index === -1) {
+        res.status(404).send("Livro não encontrado");
+        return;
+    }
+    livros[index].titulo = req.body.titulo;
+    res.status(200).send({message: "Livro atualizado com sucesso", livros});
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(+req.params.id);
+    if (index === -1) {
+        res.status(404).send("Livro não encontrado");
+        return;
+    }
+    livros.splice(index, 1);
+    res.status(200).send({message: "Livro removido com sucesso", livros});
+});
+
 export default app;
 
+// mongodb+srv://admin:<password>@cluster0.g7mhgbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
