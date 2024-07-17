27aff272cb910c4c603951337b04689718e3b07b
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-07-17 12:20:18 -0300

    :sparkles: feat: alterando mensagens de start da api

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
 import express from "express";
-import conectaNaDatabase from "./config/dbConnect.js";
-import routes from "./routes/index.js";
+import conectaNaDatabase from "./config/dbConnect.js"
+import routes from "./routes/index.js"
 
-const conexao = await conectaNaDatabase();
-
-conexao.on("error", (erro) => {
-  console.error("erro de conexão", erro);
+conectaNaDatabase().then(db => {
+  db.on("error", console.log.bind(console, 'Erro de conexão'))
+  db.once("open", () => {
+    console.log('Conexão com o banco feita com sucesso')
+  })
 });
 
-conexao.once("open", () => {
-  console.log("Conexao com o banco feita com sucesso");
-})
-
 const app = express();
+app.use(express.json())
 routes(app);
 
-export default app;
+export default app
