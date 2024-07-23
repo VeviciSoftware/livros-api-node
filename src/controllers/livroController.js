9176424d5ab6fba0907c93eab00a8409459bf03b
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-07-23 14:27:13 -0300

    :sparkles: feat: adicionando validação se o livro e autor existem

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,6 @@
 import livro from "../models/Livro.js";
 import { autor } from "../models/Autor.js";
+import NaoEncontrado from "../errors/NaoEncontrado.js";
 
 class LivroController {
 
@@ -16,16 +17,26 @@ class LivroController {
     try {
         const id = req.params.id;
         const livroEncontrado = await livro.findById(id);
+
+        if(!livroEncontrado) {
+            next(new NaoEncontrado("ID do livro não encontrado."));
+        }
+
         res.status(200).json(livroEncontrado);
     } catch (erro) {
         next(erro);
     }
-};
+  };
 
   static buscarLivrosPorAutor = async (req, res, next) => {
     const autorNome = req.query.autor;
     try {
       const autorEncontrado = await autor.find({ nome: autorNome });
+
+      if(autorEncontrado.length === 0) {
+        next(new NaoEncontrado("Autor não encontrado."));
+      }
+
       res.status(200).json(autorEncontrado);
     } catch (erro) {
       next(erro);
@@ -49,8 +60,13 @@ class LivroController {
   static atualizarLivro = async (req, res, next) => {
     try {
       const id = req.params.id;
-      await livro.findByIdAndUpdate(id, req.body);
-      res.status(200).json({ message: "Livro atualizado com sucesso." });
+      const livroAtualizado = await livro.findByIdAndUpdate(id, req.body, { new: true });
+
+      if (!livroAtualizado) {
+        next(new NaoEncontrado("ID do livro não encontrado."));
+      } else {
+        res.status(200).json({ message: "Livro atualizado com sucesso." });
+      }
     } catch (erro) {
       next(erro);
     }
@@ -59,8 +75,13 @@ class LivroController {
   static excluirLivro = async (req, res, next) => {
     try {
       const id = req.params.id;
-      await livro.findByIdAndDelete(id);
-      res.status(200).json({ message: "livro excluído com sucesso." });
+      const livroExcluido = await livro.findByIdAndDelete(id);
+
+      if (!livroExcluido) {
+        next(new NaoEncontrado("ID do livro não encontrado."));
+      } else {
+        res.status(200).json({ message: "Livro excluído com sucesso." });
+      }
     } catch (erro) {
       next(erro);
     }
