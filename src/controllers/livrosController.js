9754ed45d8532218d9fdea64904cc78f4d699cde
felipe <felipeadar108@gmail.com>
2025-01-05 13:07:31 -0300

    :sparkles: feat: ordenação  de livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -7,13 +7,19 @@ class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 6, pagina = 1 } = req.query;
+      //Campo ordenacao = campo que será utilizado para ordenar a lista de livros (titulo, por exemplo)
+      //Ordem = 1 para crescente e -1 para decrescente
+      let { limite = 6, pagina = 1, ordenacao = "_id:1" } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(":");
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
+      ordem = parseInt(ordem);
 
       if (limite > 0 && pagina > 0) {
         const livrosResultado = await livros.find()
+          .sort({ [campoOrdenacao]: ordem })
           .skip((pagina - 1) * limite)
           .limit(limite)
           .populate("autor")
