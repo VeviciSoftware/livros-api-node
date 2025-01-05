940f3c1bec66437e5878fb0551ae52a8b2486e79
felipe <felipeadar108@gmail.com>
2025-01-05 12:58:23 -0300

    :sparkles: feat: paginação de livros

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,16 +1,29 @@
 import { livros } from "../models/index.js";
 import { autores } from "../models/index.js";
 import NaoEncontrado from "../errors/NaoEncontrado.js";
+import RequisicaoIncorreta from "../errors/RequisicaoIncorreta.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      const livrosResultado = await livros.find()
-        .populate("autor")
-        .exec();
+      let { limite = 6, pagina = 1 } = req.query;
 
-      res.status(200).json(livrosResultado);
+      limite = parseInt(limite);
+      pagina = parseInt(pagina);
+
+      if (limite > 0 && pagina > 0) {
+        const livrosResultado = await livros.find()
+          .skip((pagina - 1) * limite)
+          .limit(limite)
+          .populate("autor")
+          .exec();
+
+        res.status(200).json(livrosResultado);
+      } else {
+        next(new RequisicaoIncorreta());
+      }
+
     } catch (erro) {
       next(erro);
     }
